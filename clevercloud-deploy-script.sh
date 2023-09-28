0b582ea3dfa9e767afef8405f8646e8f1b27cbd7
Clément Nivolle <clement@nivolle.com>
2023-09-28 17:03:07 +0200

    Added: script to deplot to clever cloud

---
+++ clevercloud-deploy-script.sh
@@ -0,0 +1,4 @@
+wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+chmod +x ./hugo
+./hugo --gc --minify --destination public

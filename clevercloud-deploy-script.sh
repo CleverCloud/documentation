1a5a9bac2c42f0b2e1dab901d35acb5a3948457f
Clément Nivolle <clement@nivolle.com>
2023-10-12 14:33:49 +0200

    Added :modules updates 🧩

--- clevercloud-deploy-script.sh
+++ clevercloud-deploy-script.sh
@@ -1,4 +1,5 @@
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
+./hugo mod get -u
 ./hugo --gc --minify --destination public

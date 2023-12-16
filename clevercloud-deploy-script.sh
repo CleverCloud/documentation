7cc321dd49a40e64b1c5c16997cf67216f04f56e
David Legrand <1110600+davlgd@users.noreply.github.com>
2023-12-16 07:40:16 +0100

    Update Hugo deploy script
    - Add shebang
    - Add vars at the begining of the script
    - Clean archive after download
    - Use folder in $PATH

--- clevercloud-deploy-script.sh
+++ clevercloud-deploy-script.sh
@@ -1,5 +1,14 @@
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-chmod +x ./hugo
-./hugo mod get -u
-./hugo --gc --minify --destination public
+#!/bin/bash
+
+HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+DEST_BIN="${HOME}/.local/bin/"
+FILENAME="hugo.tar.gz"
+
+wget -O ${FILENAME} -q ${HUGO_URL}
+
+mkdir -p ${DEST_BIN}
+tar xvf ${FILENAME} -C ${DEST_BIN}
+rm ${FILENAME}
+
+hugo mod get -u
+hugo --gc --minify --destination public

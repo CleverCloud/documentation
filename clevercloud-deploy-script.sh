55910e80b520364b4791ce072fc760d0134ac7aa
David Legrand <david.legrand@clever-cloud.com>
2025-06-08 12:27:21 +0200

    chore: simplify deploy script

--- clevercloud-deploy-script.sh
+++ clevercloud-deploy-script.sh
@@ -1,6 +1,4 @@
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-chmod +x ./hugo
-./hugo mod get github.com/imfing/hextra@$HEXTRA_VERSION
-./hugo --gc --minify --destination public/developers
+#! /bin/bash
+
+hugo --gc --minify
 echo "AddType text/markdown;charset=UTF-8 .md" > public/.htaccess
